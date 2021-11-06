456c2ac49afa7f2e90c68b0699cc91464a69b220
Hndz93 <88945620+Hndz93@users.noreply.github.com>
2021-11-06 08:55:04 -0600

    Create EjercisioClase.py

---
+++ EjercisioClase.py
@@ -0,0 +1,11 @@
+class  Carro:
+
+    def Encender(self):
+        return "carro encendido"
+
+    def Acelerar(self):
+        return "carro en marcha"
+
+    def Frenar(self):
+        return "frenando carro"
+
