7049fa4039dd6882db8e78700c3d33496a169f47
Hndz93 <88945620+Hndz93@users.noreply.github.com>
2021-11-13 11:00:06 -0600

    Create Parcial2.py

---
+++ Parcial2.py
@@ -0,0 +1,12 @@
+class logica :
+
+ def Mayordedosdatos (a,b):
+    
+    if a > b:
+        print(a)
+    elif a < b:
+        print(b)
+    else:
+        print()
+
+        print (Mayordedosdatos(10,5))
